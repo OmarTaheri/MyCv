5b0576caf1d2b7ee0b61f0a42285b14d14453cb3
O.Taheri@aui.ma <omartaheri2005@gmai.com>
2024-05-08 13:09:56 +0100

    style: Update CSS styles for responsive design

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -1,82 +1,94 @@
 export const experiences = [
   {
     id: 1,
-    title: "Taking my first programming course VB.NET",
-    description: "Developed key features for Google Search",
-    startDate: "AUG 5, 2015",
+    title: "Taking my first programming course: VB.NET",
+    description:
+      "My mom encouraged me to take a programming course in my small city (one of the best things she ever did for me). It was the first time I saw programming language syntax, but after finishing the course, I was ready to build an app for a school in exchange for free tuition. It was a great experience to see my app used in the school, but I wanted to learn more.",
+    startDate: "OCT 5, 2018",
   },
   {
     id: 2,
-    title: "I took a course in phothshop",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Learning Photoshop",
+    description:
+      "Feeling bored, I decided to learn something new and took a Photoshop course. I enjoyed learning this new skill and started making designs for friends for 50dh each. It was exciting to see my designs used in the real world, especially on my friends' social media channels.",
+    startDate: "JUL 5, 2019",
   },
   {
     id: 3,
-    title: "I start freelancing in khamsat",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Freelancing on Khamsat",
+    description:
+      "With a newfound focus on earning money online, I started freelancing on Khamsat. As I wasn't even 15 years old, I used this account: https://khamsat.com/user/octopusdesigns/reviews. I managed to earn 80$ in 2 weeks, even transferring 20$ to my mom's bank account just to confirm it was real (I know, I was young!). I stopped because 80$ wasn't enough, so I saved the remaining 60$ in a PayPal account while improving my programming skills.",
+    startDate: "FEB 23, 2020",
   },
   {
     id: 4,
-    title: "I start my first website (Wordpress) linarba7.com",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Launching my first website: linarba7.com (Wordpress)",
+    description:
+      "Investing 45$ of my earnings, I bought a 1-year hosting plan with a domain name. Inspired by videos about making money online, I started this blog and wrote over 20 articles, averaging 800 words each in Arabic. I learned to manage a Wordpress website, link domain names, and other related skills.",
+    startDate: "AUG 2, 2020",
   },
   {
     id: 5,
-    title: "I write my first book",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Writing my first book",
+    description:
+      "To gather emails from website visitors, I planned to offer a free book in exchange for their email addresses.  I wrote a book about making money online, titled: جميع طرق الربح من الانترنت في كتاب واحد. After closing my website, I published the book for free on various websites. You can find it by searching the title on Google.",
+    startDate: "MAR 17, 2021",
   },
   {
     id: 6,
-    title: "I start my second website it was a landing page html/css",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Building my second website: a landing page with HTML/CSS",
+    description:
+      "My next website was a landing page offering free coins in exchange for emails and completing CPA offers. I built several similar websites, and people even started copying my landing pages. While it was exciting, I eventually stopped due to insufficient income and school commitments.",
+    startDate: "MAR 17, 2021",
   },
   {
     id: 7,
-    title: "I made my first 20K in web in general.",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Earning my first 20K online",
+    description:
+      "One day, I logged into my cpaBuild account and discovered 400$. Initially thinking it was an error, I found one of my websites had unexpectedly received 2000 daily visitors for the past 3 days. I capitalized on this by creating more landing pages and earned 20K in a year. However, I stopped because I wasn't passionate about this work and wanted to delve deeper into programming.",
+    startDate: "SEP 7, 2021",
   },
   {
     id: 8,
-    title: "I start a new project (Udemy)",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
-  },
-  {
-    id: 8,
-    title: "I start a new project crypto trading group",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Starting a Udemy course sharing project",
+    description:
+      "My SEO skills gained recognition in online communities. I connected with Walled (now a close friend) and we decided to build a project together. We created a network of websites that shared free Udemy courses using AI, which generated significant income.  We stopped due to Google's DMCA policy, leading to page removals. I learned a lot from managing a dedicated server with 16 Wordpress websites and assisting Walled with script development.",
+    startDate: "JUN 2019",
   },
   {
     id: 9,
-    title: "I became an AUI student",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Launching a crypto trading signal group",
+    description:
+      "While Walled dealt with personal issues, I started a Telegram group sharing crypto signals from various sources. It aggregated signals from 20 premium groups and provided potential profit calculations, categorizing the best signal sources. The group didn't generate substantial income due to my focus on school, but I had 5 members paying 40$ monthly. You can find old posts here: https://twitter.com/arabcryptoelite",
+    startDate: "JAN 4, 2023",
   },
   {
     id: 10,
-    title:
-      "I a streaming service project (i counldnt share the link of the main website)",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Becoming an AUI student",
+    description:
+      "My good grades in the Baccalaureate exam earned me a scholarship to Al Akhawayn University (AUI). It was my preferred choice as the only university in Morocco offering an American study system.",
+    startDate: "OCT 27, 2023",
   },
   {
     id: 11,
-    title: "I became a part of mechatronique club in the unversity",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Developing a streaming service project",
+    description:
+      "This is my largest project, with over 20k lines of code across the API, backend, and frontend. The idea is a streaming service that companies can easily utilize outside the Google ecosystem.  A friend needed a similar project, so I earned 50% from his project by working on mine, a win-win situation.",
+    startDate: "OCT 29, 2023",
   },
   {
     id: 12,
-    title: "I win in a cicada game at the unversity",
-    description: "Led the development of new features for Azure",
-    startDate: "2019-06",
+    title: "Joining the Mechatronics Club at AUI",
+    description:
+      "I became a member of the Mechatronics Club at the university.",
+    startDate: " NOV 14, 2023",
+  },
+  {
+    id: 13,
+    title: "Winning the Cicada game at AUI",
+    description:
+      "Initially thinking I'd finish it in a day, it took our team of 3 six days to complete the Cicada game. I loved the problem-solving aspects, especially using programming to solve the maze.  I made new friends and improved my problem-solving skills.",
+    startDate: "APR 24, 2024",
   },
 ];
 export const skills = [
